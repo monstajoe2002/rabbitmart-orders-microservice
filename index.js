34e43f5f8b7f4ad03f5a7d2a3851d00e48041c06
monstajoe2002 <youssef.amr90@gmail.com>
2022-05-26 14:07:25 +0200

    added order status to document

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const { ObjectId } = require('mongodb');
 const mongoose=require('mongoose');
 const { mongoClient } = require('./mongo');
 const port = 3000 || 8000
@@ -7,11 +8,14 @@ const app = express()
 
 const ordersSchema = new mongoose.Schema({
     name: String,
-    order_date: Date
+    order_date: Date,
+    products:Array,
+    status:String
 })
 const Order=mongoose.model('orders',ordersSchema )
+const orderStatus = ['CREATED', 'PROCESSING', 'FULFILLED', 'CANCELED']
 //test using a document
-const order=new Order({name:'Slim',order_date:new Date()})
+const order = new Order({ name: 'balabizooo', order_date: new Date(), products: ['Lotus Biscoffs', 'El Doha Rice 1kg'], status: orderStatus[0]})
 //Routes using the CRUD model
 app.get('/api/orders', async (req, res) => {
     const db = await mongoClient();
@@ -30,7 +34,7 @@ app.post('/api/orders', async(req, res) => {
 app.put('/api/orders/:id',async (req, res) => {
     const db = await mongoClient();
     if (!db) res.status(500).send('Systems Unavailable');
-    await db.collection('orders').updateOne({name:'Slim'},{$set:{name:'balabizo'}})
+    await db.collection('orders').updateOne({ _id: ObjectId(`${req.params.id}`) }, { $set: { status: orderStatus[1]}})
     res.status(200).json({ msg: `Update order no.${req.params.id}` })
 })
 
