7431f436b66717109902a6e49cd8d419465c72eb
monstajoe2002 <youssef.amr90@gmail.com>
2022-05-27 18:16:19 +0200

    accepts json files as documents

--- index.js
+++ index.js
@@ -5,20 +5,26 @@ const { mongoClient } = require('./mongo');
 const port = 3000 || 8000
 const app = express()
 
+app.use(express.json())
 //status array
 const orderStatus = ['CREATED', 'PROCESSING', 'FULFILLED', 'CANCELED']
-//test using a document
-const order ={ name: 'Slim', order_date: new Date(), products: ['Lotus Biscoffs', 'El Doha Rice 1kg','Rawabi fala7i'], status: orderStatus[0]}
 //Routes using the CRUD model
+app.get('/api/orders/:id', async (req, res) => {
+    const db = await mongoClient();
+    if (!db) res.status(500).send('Systems Unavailable');
+    const allData = await db.collection('orders').findOne({_id:ObjectId(`${req.params.id}`)})
+    
+    res.status(200).json({ msg: 'Get order',allData })
+})
 app.get('/api/orders', async (req, res) => {
     const db = await mongoClient();
     if (!db) res.status(500).send('Systems Unavailable');
     const allData = await db.collection('orders').find().toArray();
-    
-    res.status(200).json({ msg: 'Get order',allData })
+
+    res.status(200).json({ msg: 'Get order', allData })
 })
 app.post('/api/orders', async(req, res) => {
-    
+    const order=req.body;
     const db = await mongoClient();
     if (!db) res.status(500).send('Systems Unavailable');
     await db.collection('orders').insertOne(order)
