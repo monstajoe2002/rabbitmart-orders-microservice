4f7ed1000a279060c620633ab64328c7228be68a
monstajoe2002 <youssef.amr90@gmail.com>
2022-05-31 20:42:01 +0200

    Database name added as an environment variable

--- mongo.js
+++ mongo.js
@@ -3,13 +3,13 @@ const util = require('util');
 
 util.promisify(MongoClient.connect);
 
-const { MONGO_URI} = process.env;
+const { MONGO_URI, DB_NAME} = process.env;
 let dbConnection;
 
 const connect = async () => {
     try {
         const client = await MongoClient.connect(MONGO_URI);
-        dbConnection = client.db('RabbitMartOrders');
+        dbConnection = client.db(DB_NAME);
     } catch (e) {
         throw new Error(`Could not establish database connection: ${e}`);
     }
