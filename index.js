96c51e7d64665dfe56cd15eea0c8000dd036c379
monstajoe2002 <youssef.amr90@gmail.com>
2022-05-31 20:38:24 +0200

    final change for now 😁

--- index.js
+++ index.js
@@ -9,21 +9,21 @@ app.use(express.json())
 //status array
 const orderStatus = ['CREATED', 'PROCESSING', 'FULFILLED', 'CANCELED']
 //Routes using the CRUD model
-app.get('/api/orders/:id', async (req, res) => {
+app.get('/:id', async (req, res) => {
     const db = await mongoClient();
     if (!db) res.status(500).send('Systems Unavailable');
     const allData = await db.collection('orders').findOne({_id:ObjectId(`${req.params.id}`)})
     
     res.status(200).json({ msg: 'Get order',allData })
 })
-app.get('/api/orders', async (req, res) => {
+app.get('/', async (req, res) => {
     const db = await mongoClient();
     if (!db) res.status(500).send('Systems Unavailable');
     const allData = await db.collection('orders').find().toArray();
 
     res.status(200).json({ msg: 'Get order', allData })
 })
-app.post('/api/orders', async(req, res) => {
+app.post('/', async(req, res) => {
     const order=req.body;
     const db = await mongoClient();
     if (!db) res.status(500).send('Systems Unavailable');
@@ -31,14 +31,14 @@ app.post('/api/orders', async(req, res) => {
     res.status(200).json(order)
 
 })
-app.put('/api/orders/:id',async (req, res) => {
+app.put('/:id',async (req, res) => {
     const db = await mongoClient();
     if (!db) res.status(500).send('Systems Unavailable');
     await db.collection('orders').updateOne({ _id: ObjectId(`${req.params.id}`) }, { $set: { status: orderStatus[1]}})
     res.status(200).json({ msg: `Update order no.${req.params.id}` })
 })
 
-app.delete('/api/orders/:id',async (req, res) => {
+app.delete('/:id',async (req, res) => {
     const db = await mongoClient();
     if (!db) res.status(500).send('Systems Unavailable');
     await db.collection('orders').deleteOne({ _id: ObjectId(`${req.params.id}`)})
